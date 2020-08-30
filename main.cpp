c60b8ba9520915e869f6de36e38b9edb1ffa7c7b
Hekmat Taherinejad <Hekmat.Taherinejad@skoltech.ru>
2020-08-30 20:27:29 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "include/pinhole_camera.h"
+
+
+
+
+using namespace std;
+
+
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+
+
+
+        return 0;
+}
+
+
+
