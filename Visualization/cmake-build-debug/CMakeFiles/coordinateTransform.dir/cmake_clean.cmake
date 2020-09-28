3cbdb11d247f840804473762f94afc0f565e5d9a
HekmatTaherinejad95 <Hekmat.Taherinejad@skoltech.ru>
2020-09-28 11:39:01 +0330

    Rotation Matrix to Quaternion

---
+++ Visualization/cmake-build-debug/CMakeFiles/coordinateTransform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coordinateTransform.dir/coordinateTransform.o"
+  "coordinateTransform"
+  "coordinateTransform.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coordinateTransform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Visualization/cmake-build-debug/CMakeFiles/plotTrajectory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plotTrajectory.dir/plotTrajectory.o"
+  "plotTrajectory"
+  "plotTrajectory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plotTrajectory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
