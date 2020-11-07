48414a450c158f31dfdb6a6f36f304c67dd64358
HekmatTaherinejad95 <Hekmat.Taherinejad@skoltech.ru>
2020-11-07 11:18:04 +0330

    SFM

---
+++ SFM/config.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+
+image_dir = '/home/hekmat/DATA/3D-Recon_3D-DL_Datasets/Datasets/david/'
+MRT = 0.7
+
+K = np.array([
+        [2362.12, 0, 720],
+        [0, 2362.12,  578],
+        [0, 0, 1]])
+
+
+x = 0.5
+y = 1
